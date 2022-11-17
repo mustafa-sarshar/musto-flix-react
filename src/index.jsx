7120a799138a4f62629d4d7f596a94f969436d5e
musto <musti.sar4@gmail.com>
2022-11-17 10:36:30 +0100

    Update the file based on task 3.2

--- src/index.jsx
+++ src/index.jsx
@@ -5,15 +5,12 @@ import ReactDom from "react-dom";
 // Import Styles
 import "./index.scss";
 
-// Import Components
-import MainView from "./components/mainView/mainView";
-
 // Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
     render() {
         return (
             <div className="my-flix">
-                <MainView />
+                <div>Good Morning</div>
             </div>
         );
     }
