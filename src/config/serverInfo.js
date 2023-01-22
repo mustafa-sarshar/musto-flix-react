3fa0acde33b5b550ea78853bc0a84ee42c2b967a
musto <musti.sar4@gmail.com>
2023-01-22 02:59:17 +0100

    Fix the backend address

--- src/config/serverInfo.js
+++ src/config/serverInfo.js
@@ -1,3 +1,4 @@
-const SERVER_ADDRESS = "http://ec2-44-206-243-122.compute-1.amazonaws.com";
+const SERVER_ADDRESS = "https://ec2-52-87-164-181.compute-1.amazonaws.com:443";
+
 
 export default SERVER_ADDRESS;
