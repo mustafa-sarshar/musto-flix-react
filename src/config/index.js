d547f52e29db7045f8c62bbee3e97d9c860f5ef4
musto <musti.sar4@gmail.com>
2023-02-11 22:51:59 +0100

    Update the routing

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 const SERVER_ADDRESS = "https://musto-movie-api-vf0k.onrender.com";
 const APP_ADDRESS = "/musto-flix-react";
-const APP_MODE = "dev";
+const APP_MODE = "prod";
 
 export { SERVER_ADDRESS, APP_MODE, APP_ADDRESS };
